f2bd39ae63f58642167eb8d503d175b4a3123f53
Rabia <Rabia@DESKTOP-1GEKNF6>
2024-03-06 13:05:46 +0300

    Add project files.

---
+++ Repositories/EFCore/RepositoryBase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.EFCore
+{
+    public abstract class RepositoryBase<T> : IRepositoryBase<T>
+        where T : class
+    {
+        protected readonly RepositoryContext _context;
+
+        public RepositoryBase(RepositoryContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(T t) => _context.Set<T>().Add(t);
+
+        public void Delete(T t) => _context.Set<T>().Remove(t);
+
+
+        public IQueryable<T> FindAll(bool trackChanges) =>
+            trackChanges ?
+            _context.Set<T>() : 
+            _context.Set<T>().AsNoTracking();
+
+
+        public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
+            bool trackChanges)=>
+            trackChanges ?
+            _context.Set<T>().Where(expression) :
+            _context.Set<T>().Where(expression).AsNoTracking();
+       
+
+        public void Update(T t) => _context.Set<T>().Update(t);
+
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,39 @@
+using Microsoft.EntityFrameworkCore;
+using Repositories.EFCore;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+// DbContext'in baðlanacaðý veritabaný baðlantý dizesini alýn
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// DbContext'i servis olarak ekleyin
+builder.Services.AddDbContext<RepositoryContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddControllers()
+	.AddNewtonsoftJson();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

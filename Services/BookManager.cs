2ebf5a1ae0942b76afeb5de8afffa64cdab9e3b7
Rabia <surmelirabiaa@gmail.com>
2024-04-23 15:07:13 +0300

    FilterUygulaması

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Entities.Exceptions.BadRequestException;
 
 namespace Services
 {
@@ -48,6 +49,9 @@ namespace Services
 
         public async Task<(IEnumerable<BookDto> books, MetaData metaData)> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
+            if (!bookParameters.ValidPriceRange)
+                throw new PriceOutofRangeBadRequestException();
+
             var booksWithMetaData = await _manager
                 .Book
                 .GetAllBooksAsync(bookParameters,trackChanges);

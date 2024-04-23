53f847cabdb3255211cfcfa1ae9702c484f762b3
Rabia <surmelirabiaa@gmail.com>
2024-04-23 09:31:32 +0300

    PagedList_Edit_Services

--- Services/BookManager.cs
+++ Services/BookManager.cs
@@ -46,11 +46,14 @@ namespace Services
 
         }
 
-        public async Task<IEnumerable<BookDto>> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
+        public async Task<(IEnumerable<BookDto> books, MetaData metaData)> GetAllBooksAsync(BookParameters bookParameters, bool trackChanges)
         {
-            var books = await _manager.Book.GetAllBooksAsync(bookParameters,trackChanges);
-			var bookDto = _mapper.Map<IEnumerable<BookDto>>(books);
-            return bookDto;
+            var booksWithMetaData = await _manager
+                .Book
+                .GetAllBooksAsync(bookParameters,trackChanges);
+
+			var booksDto = _mapper.Map<IEnumerable<BookDto>>(booksWithMetaData);
+            return (booksDto, booksWithMetaData.MetaData);
         }
 
         public async Task<BookDto> GetOneBookByIdAsync(int id, bool trackChanges)
